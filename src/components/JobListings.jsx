c75b12b3ca0c2464aedaf5d6bf32bcd2b51194c1
liamgoodfinity <liam@goodfinity.ca>
2025-08-26 13:23:48 -0700

    Setup json-server and told the app to fetch data from it

--- src/components/JobListings.jsx
+++ src/components/JobListings.jsx
@@ -1,8 +1,26 @@
-import jobs from '../jobs.json';
+import { useState, useEffect } from 'react'  
 import JobListing from './JobListing';
 
 const JobListings = ({ isHome = false}) => {
-    const jobListings = isHome ? jobs["jobs"].slice(0, 3) : jobs["jobs"];
+  const [jobs, setJobs] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchJobs = async () => {
+      try {
+        const res = await fetch('http://localhost:8000/jobs');
+        const data = await res.json();
+        console.log(data);
+        setJobs(data)
+      } catch (e) {
+        console.log(e);
+      } finally {
+        setLoading(false)
+      }
+    }
+    fetchJobs();
+  }, []);
+  const jobListings = isHome ? jobs.slice(0, 3) : jobs;
   return (
     <section className="bg-blue-50 px-4 py-10">
       <div className="container-xl lg:container m-auto">
@@ -10,10 +28,12 @@ const JobListings = ({ isHome = false}) => {
             {isHome ? 'Recent Job Listings' : 'All Jobs'}
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-          {/* <!-- Job Listing 1 --> */}
-          {jobListings.map((job) => (
-            <JobListing key={job.id} job={job} />
-        ))}
+          {loading ? (<h2>Loading...</h2>) : (<>
+            {jobListings.map((job) => (
+              <JobListing key={job.id} job={job} />
+            ))}
+          </>)}
+          
         </div>
       </div>
     </section>
